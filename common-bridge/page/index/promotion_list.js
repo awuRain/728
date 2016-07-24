0f06f23c81edb48037d2ffb5eab106727ba29491
andrew703 <andrew7032000@gmail.com>
2016-07-24 14:36:00 +0800

    style

---
+++ common-bridge/page/index/promotion_list.js
@@ -0,0 +1,188 @@
+module.exports = {
+    '795ac511463263cf7ae3def3': {
+        city_code: 100000000,
+        cityid: 131,
+        sname: "北京",
+        sid: '795ac511463263cf7ae3def3'
+    },
+    '289d2b074d001b3184b27ef7': {
+        city_code: 200000000,
+        cityid: 289,
+        sname: "上海",
+        sid: '289d2b074d001b3184b27ef7'
+    },
+    'b0240abb6cf9c05f301acdf3': {
+        city_code: 300000000,
+        cityid: 257,
+        sname: "广东",
+        sid: 'b0240abb6cf9c05f301acdf3'
+    },
+    '17070a5c91ca872746461bf4': {
+        city_code: 2800000000,
+        cityid: 104,
+        sname: "云南",
+        sid: '17070a5c91ca872746461bf4'
+    },
+    '7425aa3f6703cc8575ed39f0': {
+        city_code: 2600000000,
+        cityid: 125,
+        sname: "海南",
+        sid: '7425aa3f6703cc8575ed39f0'
+    },
+    'fb0af2c9c005c401f11576f3': {
+        city_code: 1500000000,
+        cityid: 127,
+        sname: "安徽",
+        sid: 'fb0af2c9c005c401f11576f3'
+    },
+    '1fdbf740851f3e07d8d23ff7': {
+        city_code: 900000000,
+        cityid: 132,
+        sname: "重庆",
+        sid: '1fdbf740851f3e07d8d23ff7'
+    },
+    '55163f0fd41f9757809972f0': {
+        city_code: 1900000000,
+        cityid: 300,
+        sname: "福建",
+        sid: '55163f0fd41f9757809972f0'
+    },
+    '64649c1ed1b18b0036d9f4f3': {
+        city_code: 3000000000,
+        cityid: 36,
+        sname: "甘肃",
+        sid: '64649c1ed1b18b0036d9f4f3'
+    },
+    'de5f301a4dadc1b44664c8f3': {
+        city_code: 2500000000,
+        cityid: 261,
+        sname: "广西",
+        sid: 'de5f301a4dadc1b44664c8f3'
+    },
+    'c2fa4653ba3ece0ae6bc95f3': {
+        city_code: 2700000000,
+        cityid: 146,
+        sname: "贵州",
+        sid: 'c2fa4653ba3ece0ae6bc95f3'
+    },
+    '4c3263cf7ae3b9d414ffdcf3': {
+        city_code: 1800000000,
+        cityid: 150,
+        sname: "河北",
+        sid: '4c3263cf7ae3b9d414ffdcf3'
+    },
+    '6275641493d83c0c3f1515f7': {
+        city_code: 1100000000,
+        cityid: 48,
+        sname: "黑龙江",
+        sid: '6275641493d83c0c3f1515f7'
+    },
+    '75285f52dec9c3f9db7dfef3': {
+        city_code: 2000000000,
+        cityid: 268,
+        sname: "河南",
+        sid: '75285f52dec9c3f9db7dfef3'
+    },
+    '9607cc8575ed2e69961327f7': {
+        city_code: 400000000,
+        cityid: 218,
+        sname: "湖北",
+        sid: '9607cc8575ed2e69961327f7'
+    },
+    '2f1a4dadc1b446649c1ec9f3': {
+        city_code: 1200000000,
+        cityid: 158,
+        sname: "湖南",
+        sid: '2f1a4dadc1b446649c1ec9f3'
+    },
+    '3a89363b4ffbffdcdfcc98f4': {
+        city_code: 700000000,
+        cityid: 315,
+        sname: "江苏",
+        sid: '3a89363b4ffbffdcdfcc98f4'
+    },
+    'c9b8ae5f1d3a784045d69ef1': {
+        city_code: 2400000000,
+        cityid: 163,
+        sname: "江西",
+        sid: 'c9b8ae5f1d3a784045d69ef1'
+    },
+    '5ec98727464640351f1219f0': {
+        city_code: 2300000000,
+        cityid: 53,
+        sname: "吉林",
+        sid: '5ec98727464640351f1219f0'
+    },
+    '052c5d285f52dec9c3f9f1f1': {
+        city_code: 1300000000,
+        cityid: 58,
+        sname: "辽宁",
+        sid: '052c5d285f52dec9c3f9f1f1'
+    },
+    'f6187498603a75a3a4c7c5f3': {
+        city_code: 2200000000,
+        cityid: 321,
+        sname: "内蒙古",
+        sid: 'f6187498603a75a3a4c7c5f3'
+    },
+    'dc65d60419efc717ac2d0af0': {
+        city_code: 3200000000,
+        cityid: 360,
+        sname: "宁夏",
+        sid: 'dc65d60419efc717ac2d0af0'
+    },
+    '7423aa3f6703cc8575ed39f6': {
+        city_code: 3100000000,
+        cityid: 66,
+        sname: "青海",
+        sid: '7423aa3f6703cc8575ed39f6'
+    },
+    '4810309ea171641493d817f6': {
+        city_code: 1400000000,
+        cityid: 288,
+        sname: "山东",
+        sid: '4810309ea171641493d817f6'
+    },
+    '1fb09a056d0754c85f197bf5': {
+        city_code: 1000000000,
+        cityid: 233,
+        sname: "陕西",
+        sid: '1fb09a056d0754c85f197bf5'
+    },
+    '68cf7ae3b9d414ff762fddf3': {
+        city_code: 2100000000,
+        cityid: 176,
+        sname: "山西",
+        sid: '68cf7ae3b9d414ff762fddf3'
+    },
+    '8e8da744ec5be32fd14c6cf7': {
+        city_code: 800000000,
+        cityid: 75,
+        sname: "四川",
+        sid: '8e8da744ec5be32fd14c6cf7'
+    },
+    'cc11463263cf7ae3b9d4dff3': {
+        city_code: 500000000,
+        cityid: 332,
+        sname: "天津",
+        sid: 'cc11463263cf7ae3b9d4dff3'
+    },
+    '295291f256c0a53ec6ad8cf7': {
+        city_code: 3300000000,
+        cityid: 92,
+        sname: "新疆",
+        sid: '295291f256c0a53ec6ad8cf7'
+    },
+    'f61b7498603a75a3a4c7c5f0': {
+        city_code: 2900000000,
+        cityid: 100,
+        sname: "西藏",
+        sid: 'f61b7498603a75a3a4c7c5f0'
+    },
+    '9520608cbb0e13551b12a1f1': {
+        city_code: 600000000,
+        cityid: 179,
+        sname: "浙江",
+        sid: '9520608cbb0e13551b12a1f1'
+    }
+};
